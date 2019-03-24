86c4917461e64ac86fba08352e4f27afe9990e38
zhurai <zhurai@users.noreply.github.com>
2019-03-24 02:49:54 -0700

    rewrite for cog

--- cogs/management.py
+++ cogs/management.py
@@ -5,24 +5,27 @@ import discord
 from discord.ext import commands
 
 class ManagementCog:
-	@bot.command()
-	async def restart(ctx):
+    def __init__(self, bot):
+        self.bot = bot
+
+	@commands.command()
+	async def restart(self, ctx):
 	    # restarts script, not watchme
 		os.startfile(cwd+'restart.bat')
-		await bot.close()
+		await self.bot.close()
 
-	@bot.command()
-	async def shutdown(ctx):
+	@commands.command()
+	async def shutdown(self, ctx):
 	    # closes script, not watchme
-	    await bot.close()
+	    await self.bot.close()
 
-	@bot.command()
-	async def close(ctx):
+	@commands.command()
+	async def close(self, ctx):
 	    # closes script, not watchme
-	    await bot.close()
+	    await self.bot.close()
 
-	@bot.command()
-	async def gitpull(ctx):
+	@commands.command()
+	async def gitpull(self, ctx):
 	    print("git pull command called")
 	    # git pulls for this script/rehash
 	    proc = subprocess.Popen([cwd+'gitpull.bat'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
