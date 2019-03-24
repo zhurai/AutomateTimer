559d1308f04bb3fa1161510ac69f90b9f5a852db
zhurai <zhurai@users.noreply.github.com>
2019-03-24 02:26:46 -0700

    take 2 on that test, forgot critical thing

--- bot.py
+++ bot.py
@@ -71,7 +71,7 @@ async def screenshot(ctx):
         await ctx.send(file=discord.File(fp,'screenshot.png'))
 
 # WATCH ME COMMANDS
-
+@bot.command()
 async def test(ctx):
 	print("this is an all new command")
 	await ctx.send("TEST!")
