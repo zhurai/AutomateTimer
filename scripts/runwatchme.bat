6ce4b6100bbd3e98cc648aa2c8ca90e4a1ca6216
zhurai <zhurai@users.noreply.github.com>
2019-03-24 16:59:11 -0700

    run watchme script

--- scripts/runwatchme.bat
+++ scripts/runwatchme.bat
@@ -1,4 +1,6 @@
 @echo off
-start "" "C:\Users\WatchMe\Google Drive\Programs-WatchMe\Files\WatchMe.exe"
+cd ..\..\
+cd WatchMe/Program
+start "" WatchMe.exe
 start "" "C:\Program Files (x86)\AutoIt3\AutoIt3.exe" startup_watchme.au3
 exit
