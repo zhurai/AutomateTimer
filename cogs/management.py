9f4b6abbfee3a70b5ce73312973050719fdc128b
zhurai <zhurai@users.noreply.github.com>
2019-03-24 03:44:01 -0700

    add aliases for commands

--- cogs/management.py
+++ cogs/management.py
@@ -10,19 +10,14 @@ class ManagementCog (commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(aliases=[reboot,reset])
     async def restart(self, ctx):
         # restarts script, not watchme
         os.startfile(scriptdir+'restart.bat')
         await self.bot.close()
 
-    @commands.command()
+    @commands.command(aliases=[close])
     async def shutdown(self, ctx):
-        # closes script, not watchme
-        await self.bot.close()
-
-    @commands.command()
-    async def close(self, ctx):
         # closes script, not watchme
         await self.bot.close()
 
@@ -36,7 +31,7 @@ class ManagementCog (commands.Cog):
         stdout_value = proc.stdout.read() + proc.stderr.read()
         await ctx.send(stdout_value)
 
-    @commands.command()
+    @commands.command(aliases=[startwatchme])
     async def runwatchme(self,ctx):
         # starts watchme
         proc = subprocess.Popen([scriptdir+'runwatchme.bat'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
