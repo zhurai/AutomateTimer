13f18b28c96f20946c107e0f576f3ea46b2b44c3
zhurai <zhurai@users.noreply.github.com>
2019-03-25 07:04:01 -0700

    test watchme start

--- scripts/runwatchme.bat
+++ scripts/runwatchme.bat
@@ -1,7 +1,5 @@
 @echo off
-cd ..\..\
-cd WatchMe/Program
-start "" WatchMe.exe
+start "" ..\..\WatchMe\Program\WatchMe.exe
 cd %~dp0
 start "" "C:\Program Files (x86)\AutoIt3\AutoIt3.exe" startup_watchme.au3
 exit
