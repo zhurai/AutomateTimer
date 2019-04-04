fb710fe4865493d74d17251f1c4103a49fa6a1ee
zhurai <zhurai@users.noreply.github.com>
2019-04-04 08:49:02 -0700

    use config.BOT/hide option

--- bot.py
+++ bot.py
@@ -22,7 +22,8 @@ async def on_ready():
     print("Username: " + bot.user.name)
     print("ID: " + str(bot.user.id))
     print("-----")
-    proc = subprocess.Popen(['C:\Program Files (x86)\AutoIt3\AutoIt3.exe',scriptdir+'startup_bot.au3'],stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    if localconfig.getint('hide') == 1:
+        proc = subprocess.Popen(['C:\Program Files (x86)\AutoIt3\AutoIt3.exe',scriptdir+'startup_bot.au3'],stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
 
 bot.load_extension("cogs.management")
 bot.load_extension("cogs.watchme")
