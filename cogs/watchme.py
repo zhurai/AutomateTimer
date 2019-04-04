4396ea421bc66ffb32080244a824731d74447558
zhurai <zhurai@users.noreply.github.com>
2019-04-04 08:46:53 -0700

    seperate !wm get/set with !wmget and !wmset (wmset TBD)

--- cogs/watchme.py
+++ cogs/watchme.py
@@ -11,180 +11,120 @@ class WatchMeCog (commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def wm(self, ctx, *args):
-        print ("wm: watchme command called with arguments: '", args,"'")
+    async def wmget(self, ctx, *args):
+        print ("wmget: watchme command called with arguments: '", args,"'")
 
         if len(args) == 0:
             # no argument given
-            print("  wm: no argument given")
-            await ctx.send("[ERROR] wm: no argument given")
+            print("  wmget: no argument given")
+            await ctx.send("[ERROR] wmget: no argument given")
 
-        # !wm get ()
-        if len(args) > 0 and args[0].lower() == 'get':
-            if len(args) > 1:
+        # !wmget ()
+        if len(args) > 1:
+            # !wmget tab ()
+            if args[0].lower() == 'tab' or args[0].lower() == 'tabs':
+                if len(args) > 2:
+                    print("  wmget.tab: arguments: " + args[2])
+                    # !wmget tab num
+                    if args[1].lower() == 'num' or args[1].lower() == 'id':
+                        proc = subprocess.Popen(['C:\Program Files (x86)\AutoIt3\AutoIt3.exe',scriptdir+'wmget.tabs.num.au3'],stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                        stdout_value = proc.stdout.read() + proc.stderr.read()
+                        await ctx.send(stdout_value.rstrip().decode())
 
+                    # !wmget tab name
+                    elif args[1].lower() == 'name':
+                        proc = subprocess.Popen(['C:\Program Files (x86)\AutoIt3\AutoIt3.exe',scriptdir+'wmget.tabs.name.au3'],stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                        stdout_value = proc.stdout.read() + proc.stderr.read()
+                        await ctx.send(stdout_value.rstrip().decode())
 
-                # !wm get tab ()
-                if args[1].lower() == 'tab' or args[1].lower() == 'tabs':
-                    if len(args) > 2:
-                        print("  wm.get.tabs: arguments: " + args[2])
+                    # !wmget tab list
+                    elif args[1].lower() == 'list':
+                        # list of tabs
+                        proc = subprocess.Popen(['C:\Program Files (x86)\AutoIt3\AutoIt3.exe',scriptdir+'wmget.tabs.list.au3'],stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                        stdout_value = proc.stdout.read() + proc.stderr.read()
+                        tabs1=stdout_value.decode().split("\r\n")
+                        tabs2="\n".join(tabs1).rstrip()
+                        await ctx.send("```"+tabs2+"```")
 
-                        # !wm get tab num
-                        if args[2].lower() == 'num' or args[2].lower() == 'id':
-                            proc = subprocess.Popen(['C:\Program Files (x86)\AutoIt3\AutoIt3.exe',scriptdir+'wmget.tabs.num.au3'],stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-                            stdout_value = proc.stdout.read() + proc.stderr.read()
-                            await ctx.send(stdout_value.rstrip().decode())
-
-                        # !wm get tab name
-                        elif args[2].lower() == 'name':
-                            proc = subprocess.Popen(['C:\Program Files (x86)\AutoIt3\AutoIt3.exe',scriptdir+'wmget.tabs.name.au3'],stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-                            stdout_value = proc.stdout.read() + proc.stderr.read()
-                            await ctx.send(stdout_value.rstrip().decode())
-
-                        # !wm get tab list
-                        elif args[2].lower() == 'list':
-                            # list of tabs
-                            proc = subprocess.Popen(['C:\Program Files (x86)\AutoIt3\AutoIt3.exe',scriptdir+'wmget.tabs.list.au3'],stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-                            stdout_value = proc.stdout.read() + proc.stderr.read()
-                            tabs1=stdout_value.decode().split("\r\n")
-                            tabs2="\n".join(tabs1).rstrip()
-                            await ctx.send("```"+tabs2+"```")
-
-
-                        # !wm get tab ???
-                        else:
-                            print("  wm.get.tabs: invalid argument")
-                            await ctx.send("[ERROR] wm.get.tabs: invalid argument given")
-                    # !wm get tab
+                    # !wmget tab ???
                     else:
-                        print("  wm.get.tabs: not enough arguments")
-                        await ctx.send("[ERROR] wm.get.tabs: not enough arguments")
+                        print("  wmget.tab: invalid argument")
+                        await ctx.send("[ERROR] wmget.tabs: invalid argument given")
 
+                # !wmget tab
+                else:
+                    print("  wmget.tabs: not enough arguments")
+                    await ctx.send("[ERROR] wmget.tabs: not enough arguments")
 
-                # !wm get timer ()
-                elif args[1].lower() == 'timer' or args[1].lower() == 'timers':
-                    if len(args) > 2:
-                        print("  wm.get.timers: arguments: " + args[2])
+            # !wmget timer ()
+            elif args[0].lower() == 'timer' or args[0].lower() == 'timers':
+                if len(args) > 2:
+                    print("  wm.get.timers: arguments: " + args[1])
 
-                        # !wm get timers curr/current
-                        if args[2].lower() == 'curr' or args[2].lower() == 'current':
-                            # list of timers on current section / tab
-                            print("  wm.get.timers: current tab")
-                            proc = subprocess.Popen(['C:\Program Files (x86)\AutoIt3\AutoIt3.exe',scriptdir+'wmget.timer.curr.au3'],stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-                            stdout_value = proc.stdout.read() + proc.stderr.read()
-                            timers=stdout_value.decode().split("\r\n")
-                            timers2=", ".join(timers).rstrip(', ')
-                            await ctx.send(timers2)
-
-                        # !wm get timers all
-                        elif args[2].lower() == 'all':
-                            # list of timers on all sections / tabs
-                            print("  wm.get.timers: all tabs")
-                            proc = subprocess.Popen(['C:\Program Files (x86)\AutoIt3\AutoIt3.exe',scriptdir+'wmget.timer.all.au3'],stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-                            stdout_value = proc.stdout.read() + proc.stderr.read()
-                            timers=stdout_value.decode().split("\r\n")
-                            timers2=", ".join(timers).rstrip(', ')
-                            await ctx.send(timers2)
-
-
-                        # !wm get timers ???
-                        else:
-                            print("  wm.get.timers: invalid argument")
-                            await ctx.send("[ERROR] wm.get.timers: invalid argument given")
-                    else:
-                        # only display CURRENT tab timers by default
-                        print("  wm.get.timers: default (current tab only)")
+                    # !wm get timers curr/current
+                    if args[1].lower() == 'curr' or args[1].lower() == 'current':
+                        # list of timers on current section / tab
+                        print("  wmget.timers: current tab")
                         proc = subprocess.Popen(['C:\Program Files (x86)\AutoIt3\AutoIt3.exe',scriptdir+'wmget.timer.curr.au3'],stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
                         stdout_value = proc.stdout.read() + proc.stderr.read()
-                        tabs=stdout_value.decode().replace("\r\n",", ")
-                        await ctx.send(tabs)
+                        timers=stdout_value.decode().split("\r\n")
+                        timers2=", ".join(timers).rstrip(', ')
+                        await ctx.send(timers2)
 
-                elif args[1].lower() == 'control' or args[1].lower() == 'controls':
-                    if len(args) > 2:
-                        print("  wm.get.control: arguments: " + args[2])
-
-                        # !wm get control list
-                        # output all CURRENT tab timers
-                        if args[2].lower() == 'list' or args[2].lower() == 'current':
-                            proc = subprocess.Popen(['C:\Program Files (x86)\AutoIt3\AutoIt3.exe',scriptdir+'wmget.control.list.au3'],stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-                            stdout_value = proc.stdout.read() + proc.stderr.read()
-                            control1=stdout_value.decode().split("\r\n")
-                            control2="\n".join(control1).rstrip()
-                            await ctx.send("```"+control2+"```")
+                    # !wm get timers all
+                    elif args[1].lower() == 'all':
+                        # list of timers on all sections / tabs
+                        print("  wmget.timers: all tabs")
+                        proc = subprocess.Popen(['C:\Program Files (x86)\AutoIt3\AutoIt3.exe',scriptdir+'wmget.timer.all.au3'],stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                        stdout_value = proc.stdout.read() + proc.stderr.read()
+                        timers=stdout_value.decode().split("\r\n")
+                        timers2=", ".join(timers).rstrip(', ')
+                        await ctx.send(timers2)
 
 
-                        # !wm get control "text"
-                        # output "text" "start/stop control" "reset control"
+                    # !wm get timers ???
+                    else:
+                        print("  wmget.timers: invalid argument")
+                        await ctx.send("[ERROR] wmget.timers: invalid argument given")
 
-
-                # !wm get ???
                 else:
-                    print("  wm.get: invalid argument")
-                    await ctx.send("[ERROR] wm.get: invalid argument given")
-            # !wm get
+                    # only display CURRENT tab timers by default
+                    print("  wmget.timers: default (current tab only)")
+                    proc = subprocess.Popen(['C:\Program Files (x86)\AutoIt3\AutoIt3.exe',scriptdir+'wmget.timer.curr.au3'],stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                    stdout_value = proc.stdout.read() + proc.stderr.read()
+                    tabs=stdout_value.decode().replace("\r\n",", ")
+                    await ctx.send(tabs)
+
+            # !wmget control ()
+            elif args[0].lower() == 'control' or args[0].lower() == 'controls':
+                if len(args) > 2:
+                    print("  wmget.control: arguments: " + args[1])
+
+                    # !wmget control list
+                    # output all CURRENT tab timers
+                    if args[1].lower() == 'list' or args[1].lower() == 'current':
+                        proc = subprocess.Popen(['C:\Program Files (x86)\AutoIt3\AutoIt3.exe',scriptdir+'wmget.control.list.au3'],stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                        stdout_value = proc.stdout.read() + proc.stderr.read()
+                        control1=stdout_value.decode().split("\r\n")
+                        control2="\n".join(control1).rstrip()
+                        await ctx.send("```"+control2+"```")
+
+                    # !wm get control "text"
+                    # output "text" "start/stop control" "reset control"
+            # !wm get ???
             else:
-                # no argument given
-                print("  wm.get no argument given")
-                await ctx.send("[ERROR] wm.get: not enough arguments")
-
-
-
-
-        #elif len(args) > 0 and args[0].lower() == 'set':
-        #    None
-
-
-
-
-        elif len(args) > 0 and args[0].lower() == 'add':
-            if len(args) > 1:
-                # !wm add tab ()
-                if args[1].lower() == 'tab':
-                    None
-                #elif args[1].lower() == '':
-                #    None
-
-
-        # !wm ()
+                print("  wmget: invalid argument")
+                await ctx.send("[ERROR] wmget: invalid argument given")
+        # !wm get
         else:
-            # invalid arguments
-            print("  wm: invalid arguments")
-            await ctx.send("[ERROR] wm: invalid arguments")
+            # no argument given
+            print("  wmget no argument given")
+            await ctx.send("[ERROR] wmget: not enough arguments")
 
-
-
-        '''
-                if args[1].lower()=='timers':
-                    None
-
-                elif args[1].lower()=='control':
-                    None
-
-                elif args[1].lower()=='control':
-                    None
-
-        elif len(args) == 1:
-            # one argument given
-            print("  git: argument given: '" + args[0],"'")
-            if (args[0].lower() == 'watchme' or args[0].lower() == 'app'):
-                # watchme itself
-                print("   git: watchme selected")
-                await ctx.send("git: pulling from watchmescripts")
-                proc = subprocess.Popen([scriptdir+'gitpullwatchme.bat'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-                stdout_value = proc.stdout.read() + proc.stderr.read()
-                await ctx.send(stdout_value.rstrip().decode())
-            elif (args[0].lower() == 'automatetimer' or args[0].lower() == 'self' or args[0].lower() == 'srv' or args[0].lower() == 'bot'):
-                # automate timer
-                print("   git: automatetimer selected")
-                await ctx.send("git: pulling from automatetimer")
-                proc = subprocess.Popen([scriptdir+'gitpull.bat'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-                stdout_value = proc.stdout.read() + proc.stderr.read()
-                await ctx.send(stdout_value.rstrip().decode())
-            else:
-                print("   git: none selected")
-                await ctx.send("git: invalid input!")
-        print(" git: finished")
-        '''
+    @commands.command()
+    async def wmset(self, ctx, *args):
+        print ("wmset: [NI] watchme command called with arguments: '", args,"'")
+        await ctx.send("[ERROR] wmset: not implemented yet")
 
 def setup(bot):
     bot.add_cog(WatchMeCog(bot))
