a439d136cf7efd2d8189e07b71eb900ff1b6a2f3
zhurai <zhurai@users.noreply.github.com>
2019-04-08 08:54:40 -0700

    on startup, scrape + fixed sleep

--- cogs/pad.py
+++ cogs/pad.py
@@ -26,17 +26,54 @@ class PADCog (commands.Cog):
             padtime_scrapeskyozora = '08:01'
         elif time.localtime().tm_isdst == 0:
             padtime_scrapeskyozora = '07:01'
-        dailies_time = []
+
+        channel = self.bot.get_channel(channelid)
+        dst = time.localtime().tm_isdst
+        dailies_time=[]
         dailies_string=[]
+
+        # scrape at startup
+        ################################################
+        r=requests.get("http://pad.skyozora.com/")
+        soup = BeautifulSoup(r.content, 'html.parser')
+        tables = soup.find("table","sample")
+        rows = tables.find_all('tr')
+
+        for i,row in enumerate(rows):
+            if i > 1:
+                cols = row.find_all('td')
+                cols2 = [ele.text.strip() for ele in cols]
+                if i == 2:
+                    descends=cols2[1].split(" ")
+                    message = "\n".join(descends).rstrip()
+                    message = "Today's Descends\n" + message
+                    await channel.send("```"+message+"```")
+                elif i == len(rows)-1 or i == len(rows)-2:
+                    None
+                else:
+                    offset=0
+                    if dst==0:
+                        offset=16
+                    else:
+                        offset=15
+                    time = int(cols2[0][:-1])-offset
+                    if time < 0:
+                        time=time+24
+                        time=str('{0:02d}'.format(time))+":00"
+                        dailies_time.append(time)
+                        name=row.find_all('td')[0].find('a')["title"]
+                        dailies_string.append(name)
+
+            dailies="Today's Guerrilla Schedule\n"
+            for i,row in enumerate(dailies_time):
+                dailies = dailies+row+" "+dailies_string[i]+"\n"
+            await channel.send("```"+dailies+"```")
+        ################################################
 
         while self is self.bot.get_cog("PADCog"):
             now = datetime.strftime(datetime.now(),'%H:%M')
             if now == padtime_scrapeskyozora:
                 # scrape current events
-                channel = self.bot.get_channel(channelid)
-                dst = time.localtime().tm_isdst
-                dailies_time=[]
-                dailies_string=[]
 
                 r=requests.get("http://pad.skyozora.com/")
                 soup = BeautifulSoup(r.content, 'html.parser')
@@ -80,10 +117,7 @@ class PADCog (commands.Cog):
                     padtime_scrapeskyozora = '08:01'
                 elif time.localtime().tm_isdst == 0:
                     padtime_scrapeskyozora = '07:01'
-
-                # set timers for it
-
-                await asyncio.sleep(90)
+                await asyncio.sleep(61)
             else:
                 await asyncio.sleep(1)
 
